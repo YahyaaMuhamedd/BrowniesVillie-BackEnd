24dca977b5a88a1d18ad5554691a4fefba66d47c
MOOS <144733181+YahyaaMuhamedd@users.noreply.github.com>
2025-01-02 19:06:46 +0200

    Fix and Add CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const productsRoute = require("./routes/products.route");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require("mongoose");
@@ -7,7 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/products", productsRoute);
 
 
